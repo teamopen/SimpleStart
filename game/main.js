63157dc91250eb4baaba233f23dd9f888a505f7c
Din Ferizovic <din.ferizovic@gmail.com>
2015-06-19 23:26:57 +0000

    Added:boing sound that goes boing and score

--- game/main.js
+++ game/main.js
@@ -15,14 +15,18 @@ function preload(){
   game.load.image('4', 'assets/img/element_green_square.png');
   game.load.image('5', 'assets/img/element_red_square.png');
   game.load.image('6', 'assets/img/element_blue_square.png');
+  //Collision sound
+  game.load.audio('boing', 'assets/snd/phaserUp3.ogg');
   
 }
-
+var score = 0;
+var scoreText;
 var blocks;
 var player;
 var ball;
 var cursors;
-
+var boing;
+var defeat;
 function create(){
   // Game initialization
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -55,7 +59,10 @@ function create(){
   blocks.physicsBodyType = Phaser.Physics.ARCADE;
   
   boardgen();
-
+  //Initialize the sound
+  boing = game.add.audio('boing');
+  //Score
+  scoreText = game.add.text(10, 600, 'Score: 0', { fontSize: '32px', fill: '#FFFFFF' });
 }
 
 function update(){
@@ -83,6 +90,8 @@ function BallHitsPlayer() { };
 
 function BallHitsBlock(_ball, _block) {
   _block.kill();
+  boing.play();
+  updateScore();
 }
 
 function randomBlock() {
@@ -143,6 +152,13 @@ function GameOver() {
   ball.body.velocity.x = 0;
   ball.body.velocity.y = 0;
   
-  game.add.text(game.world.centerX, game.world.centerY, 'You lost!', {font: '28px Arial', fill: '#ff0000', align: 'center'});
+  game.add.text(game.world.centerX, game.world.centerY, 'You lost! Final Score: '+score, {font: '28px Arial', fill: '#ff0000', align: 'center'});
+
+}
+
+function updateScore() {
+  
+  score += 10;
+  scoreText.text = 'Score: ' + score;
   
 }
