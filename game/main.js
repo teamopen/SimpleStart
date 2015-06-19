0460d1982ad1b192e65dbffd89826f8c2cc31b93
pehowell <peter.e.howell@gmail.com>
2015-06-19 14:48:34 +0000

    some more code cleanup

--- game/main.js
+++ game/main.js
@@ -1,5 +1,5 @@
-/*Global Phaser*/
-//First we define our Basic game container variable. This is basic boilerplate.
+/*global Phaser*/
+//First we define our basic game container variable. This is basic boilerplate.
 //Args: width, height, renderer, and div in the hosting page.
 var game = new Phaser.Game(480,640, Phaser.AUTO, 'game_div', {preload: preload, create: create, update: update});
 
@@ -7,18 +7,25 @@ var game = new Phaser.Game(480,640, Phaser.AUTO, 'game_div', {preload: preload, 
 function preload(){
   game.load.image('bat', 'assets/img/paddleBlu.png');
   game.load.image('ball', 'assets/img/ballBlue.png');
-  game.load.image('greenBlock','assets/img/element_green_rectangle.png')
+  game.load.image('greenBlock','assets/img/element_green_rectangle.png');
 }
 
 var blocks;
+var player;
 
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  
+  //Initialize the Player's paddle
+  player = game.add.sprite(0, 0, 'bat');
+  player.x = (game.world.width / 2) - (player.width / 2);
+  player.y = (game.world.height - 128);
+  
+  //Initialize the blocks
   blocks = game.add.group();
   blocks.enableBody = true;
-  player = game.add.sprite(game.world.width / 2 - 52, game.world.height - 132, 'bat');
-
 }
+
 function update(){
 
 }
